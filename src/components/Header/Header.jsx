6a99468aecfac1bff8ea3373707c4c45c764e6dc
Clueroi <eriicsouzaromeroo@gmail.com>
2023-04-21 13:59:19 -0300

    Component icone

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,19 @@
 import Title from '../../components/Title/Title'
 import Subtitle from '../Subtitle/Subtitle'
 import styles from './header.module.css'
-
+import Icon from '../icon/Icon'
 
 function Header(){
   return(
+    <>
     <div className={styles.Header}>
        <Title> Jogo da Velha</Title>
         <Subtitle>Criado por Eric</Subtitle>
     </div>
+    <div className={styles.IconContent}>
+      <Icon iconName="github" link="https://github.com/Clueroi"></Icon>
+    </div>
+    </>
   )
 }
 
